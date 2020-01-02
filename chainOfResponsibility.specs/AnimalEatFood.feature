fc49adea045c2bcef5d18442656f118ac6d711fd
Francisco Vasquez <francisco.javiervazquez@tui.be>
2020-01-02 17:40:17 +0100

    Adding spec flow to the code.

---
+++ chainOfResponsibility.specs/AnimalEatFood.feature
@@ -0,0 +1,21 @@
+﻿Feature: AnimalEatFood
+	In order to select the animal for each food
+	As a Zoo Worker 
+	I want to know which animal eat what food
+
+Scenario: The Monkey will eat when I pass Banana
+	Given I'm a Zoo Worker
+	When I throw a Banana
+	Then the animal who picked the food should be Monkey
+
+
+Scenario: The Dog will eat when I pass MeatBall
+	Given I'm a Zoo Worker
+	When I throw a MeatBall
+	Then the animal who picked the food should be Dog
+
+	
+Scenario: The Squirrel will eat when I pass Nut
+	Given I'm a Zoo Worker
+	When I throw a Nut
+	Then the animal who picked the food should be Squirrel
---
+++ chainOfResponsibility.specs/AnimalEatFoodSteps.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using responsibilityPattern;
+using responsibilityPattrn.Handler.validators;
+using startingTestconsoleApp.Models;
+using startingTestconsoleApp.Validators;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace chainOfResponsibility.specs
+{
+    [Binding]
+    public class AnimalEatFoodSteps
+    {
+        private Animal _animal;
+        private MonkeyHandler _monkeyHandler;
+        private SquirrelHandler _squirrelHandler;
+        private DogHandler _dogHandler;
+
+        [Given(@"I'm a Zoo Worker")]
+        public void GivenImaZooWorker()
+        {
+            _animal = new Animal();
+        }
+
+        [When(@"I throw a (.*)")]
+        public void WhenIThrowFood(string p0)
+        {
+            _animal.Food = p0;
+        }
+
+        [Then(@"the animal who picked the food should be (.*)")]
+        public void ThenTheCorrectAnimalWillEatTheFood(string p0)
+        {
+            _animal.Specie = p0;
+
+            switch (p0)
+            {
+                case "Dog":
+                    _dogHandler = new DogHandler();
+                    Assert.Equal($"{p0}: I'll eat the {_animal.Food}.\n", _dogHandler.Handle(_animal));
+                    break;
+                case "Monkey":
+                    _monkeyHandler = new MonkeyHandler(); ;
+                    Assert.Equal($"{p0}: I'll eat the {_animal.Food}.\n", _monkeyHandler.Handle(_animal));
+                    break;
+                case "Squirrel":
+                    _squirrelHandler = new SquirrelHandler();
+                    Assert.Equal($"{p0}: I'll eat the {_animal.Food}.\n", _squirrelHandler.Handle(_animal));
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+
+        }
+
+    }
+}
